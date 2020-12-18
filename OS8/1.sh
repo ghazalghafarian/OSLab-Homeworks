3fcaa249391d4e28b3c9b6c753e14dac9e8c4fce
ghzlghfrian <qzll.qfrian@gmail.com>
2020-12-18 23:27:46 +0330

    OS8

--- OS8/1.sh
+++ OS8/1.sh
@@ -1,5 +1,4 @@
 cd Downloads/OSLab-Homeworks/
 git config --global user.email "qzll.qfrian@gmail.com"
 git config --global user.name "ghzlghfrian"
-git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=86400'
