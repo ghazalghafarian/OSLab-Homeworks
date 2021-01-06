43fdf3f5cb50c2bb4315bfeadd8e6edaa1bcc611
ghzlghfrian <qzll.qfrian@gmail.com>
2021-01-06 21:41:32 +0330

    OS10

---
+++ OS10/1.sh
@@ -0,0 +1,7 @@
+cd Downloads/OSLab-Homeworks/OS10
+nano party.sh
+
+chmod +x party.sh
+./party.sh 26
+./party.sh 17 yes
+./party.sh 17 no
---
+++ OS10/party.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $1 -gt 18 ]
+then
+echo You may go to the party.
+else
+if [ $2 == "yes" ]
+then 
+echo You may go to the party but be back before midnight.
+elif [ $2 == "no" ]
+then
+echo You may not go to the party.
+fi
+fi
